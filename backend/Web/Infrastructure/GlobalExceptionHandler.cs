b159aa1b8e15dfdb26b6c7b3f042a333c0f6caba
EmilzonJ <emilzonujero@gmail.com>
2024-06-15 14:11:31 -0600

    DELETE: Controller example created by default

--- backend/Web/Infrastructure/GlobalExceptionHandler.cs
+++ backend/Web/Infrastructure/GlobalExceptionHandler.cs
@@ -15,7 +15,7 @@ public sealed class GlobalExceptionHandler(
     {
         logger.LogError(exception, "Exception occurred: {Message}", exception.Message);
 
-        var problemDetails = new ProblemDetails()
+        var problemDetails = new ProblemDetails
         {
             Status = StatusCodes.Status500InternalServerError,
             Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
